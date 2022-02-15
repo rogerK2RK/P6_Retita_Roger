3ba0b5f0185ca6bb869046db5343db37ac1fd814
Roger Retita <roger.retita@gmail.com>
2022-02-15 16:20:59 +0100

    lire le photographer

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,41 +1,40 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
         let url = '../../data/photographers.json';
-
-        fetch(url).then(function(reponse) {
-            if (reponse.ok) {
+        fetch(url)
+        .then(function(reponse) {
+            if (reponse.ok){
                 return reponse.json();
             }
         })
         .then(function(data) {
-            let photographers = data.photographers;
-            console.log(photographers);
+            let photographe = data.photographers;
+            console.log(photographe);
         })
         .catch(function(err) {
             console.log(err);
          });
-        
         // et bien retourner le tableau photographers seulement une fois
-         return ({
-             photographers: [...photographers]
-            })
+        return ({
+            photographers: [...photographers]
+        })
     }
-getPhotographers();
-    // async function displayData(photographers) {
-    //     const photographersSection = document.querySelector(".photographer_section");
+// getPhotographers();
+    async function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer_section");
 
-    //     photographers.forEach((photographer) => {
-    //         const photographerModel = photographerFactory(photographer);
-    //         const userCardDOM = photographerModel.getUserCardDOM();
-    //         photographersSection.appendChild(userCardDOM);
-    //     });
-    // };
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
+        });
+    };
  
-    // async function init() {
-    //     // Récupère les datas des photographes
-    //     const { photographers } = await getPhotographers();
-    //     displayData(photographers);
-    // };
+    async function init() {
+        // Récupère les datas des photographes
+        const { photographers } = await getPhotographers();
+        displayData(photographers);
+    };
     
-    // init();
+    init();
     
