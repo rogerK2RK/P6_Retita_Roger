593b286a22f4706495ec96484dc4a879b6844f9c
Roger Retita <roger.retita@gmail.com>
2022-02-16 12:31:07 +0100

    tentative de correction

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,40 +1,38 @@
 async function getPhotographers() {
     // Penser à remplacer par les données récupérées dans le json
     
-    let photographers = '../../data/photographers.json';
-    fetch(photographers)
-    .then(function(photographers) {
-        if (photographers.ok){
-            return photographers.json();
+    let url = 'http://localhost/data/data.json';
+    fetch(url)
+    .then(function(reponse) {
+        if (reponse.ok){
+            return reponse.json();
         }
     })
     .then(function(data) {
+        console.log(data);
         let photographe = data.photographers;
         console.log(photographe);
-        for(let user of photographe){
-            console.log(`${user.name}`);
-        }
     })
     .catch(function(err) {
         console.log(err);
-        });
+    });
 }
 
 getPhotographers();
-// async function displayData(photographers) {
-//     const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-//     photographers.forEach((photographer) => {
-//         const photographerModel = photographerFactory(photographer);
-//         const userCardDOM = photographerModel.getUserCardDOM();
-//         photographersSection.appendChild(userCardDOM);
-//     });
-// };
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
 
-// async function init() {
-//     // Récupère les datas des photographes
-//     const { photographers } = await getPhotographers();
-//     displayData(photographers);
-// };
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
 
-// init();
+init();
