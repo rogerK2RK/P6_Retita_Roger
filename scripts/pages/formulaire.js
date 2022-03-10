763356f24cca0cfa62d53de75e7cd7fc2a4fd5d6
Roger Retita <roger.retita@gmail.com>
2022-03-10 15:51:34 +0100

    form tentative

--- scripts/pages/formulaire.js
+++ scripts/pages/formulaire.js
@@ -1,14 +1,9 @@
+
 // Prenom
-const prenomUser = document.getElementById("first");
-// Nom
-const nomUser  = document.getElementById("last");
-// email
-const emailUser = document.getElementById("email");
+var prenomUser = document.getElementById("first").value;
 
 // quand l'utilisateur appui sur submit
 const submitForm = document.getElementById("contact_button-form")
 submitForm.addEventListener("click", function(e){
     console.log(prenomUser);
-    console.log(nomUser);
-    console.log(emailUser);
 });
